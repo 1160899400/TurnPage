5c4a6547410f559b8921d9d0e0962f2c5109b2fe
1160899400 <1160899400@qq.com>
2018-08-14 09:21:03 +0800

    Initial commit

---
+++ app/src/main/java/com/royole/demo/bean/MyPoint.java
@@ -0,0 +1,23 @@
+package com.royole.demo.bean;
+
+/**
+ * Created by anlia on 2017/10/19.
+ */
+
+public class MyPoint {
+    public float x, y;
+
+    public MyPoint() {
+    }
+
+    public MyPoint(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
